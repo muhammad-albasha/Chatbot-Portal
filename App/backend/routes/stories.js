ddfc75f828a098f6bd0a6977ef3520ce8bc55de8
Muhammad <1737353@uni-wuppertal.de>
2024-01-04 20:39:23 +0100

    Add update route for steps in stories

--- App/backend/routes/stories.js
+++ App/backend/routes/stories.js
@@ -4,6 +4,7 @@ import Story from '../models/Story.js';
 import Step from '../models/Step.js';
 import Response from '../models/Response.js';
 import authMiddleware from '../routes/authMiddleware.js';
+import mongoose from 'mongoose';
 
 
 router.get('/', authMiddleware, async (req, res) => {
@@ -24,29 +25,60 @@ router.get('/', authMiddleware, async (req, res) => {
     }
 });
 
-router.put('/steps/:stepId', authMiddleware, async (req, res) => {
-    const { stepId } = req.params;
+router.put('/:storyId/:stepId', authMiddleware, async (req, res) => {
+    const { storyId, stepId } = req.params;
     const { userId } = req.user;
     const stepUpdates = req.body;
+    const newStep = req.body;
+    const newResponse = req.body.response;
+    try {
+        const storyData = await Story.findById(storyId);
+        const stepData = storyData.steps.id(stepId);
+        const updatedResponse = await Response.findOneAndUpdate({ intent: newResponse.intent }, newResponse);
+        storyData.steps = storyData.steps.map((step) => {
+            return step._id == stepId ? newStep : step;
+        });
+        const updatedStory = await Story.findOneAndUpdate({ _id: new mongoose.Types.ObjectId(storyId) }, storyData);
+
+        if(updatedStory){
+            res.status(200).json({ message: 'Step erfolgreich aktualisiert', step: updatedStory });
+        } else {
+            res.status(404).json({ message: 'Step nicht gefunden' });
+        }
+    } catch (error) {
+        console.error('Fehler beim Aktualisieren des Steps:', error);
+        res.status(500).json({ message: 'Serverfehler' });
+    }
+});
+
+router.put('/steps/:stepId', authMiddleware, async (req, res) => {
+    const { stepId } = req.params;
+    console.log("🚀 ~ stepId:", stepId);
+    const { userId } = req.user;
+    const stepUpdates = req.body;
+    const { response } = req.body;
 
     try {
-        const story = await Story.findOne({ 'steps._id': stepId, user_id: userId });
-
-        if (!story) {
+        const stepData = await Step.findById(stepId);
+        console.log("🚀 ~ story:", stepData);
+        const updateResponse = await Response.findOneAndUpdate({ intent: response.intent }, response
+            // , { upsert: true, new: true }
+        );
+        console.log("🚀 ~ updateResponse:", updateResponse);
+        if (!stepData) {
             return res.status(404).json({ message: 'Story nicht gefunden oder Zugriff verweigert' });
         }
 
-        const step = story.steps.id(stepId);
-        if (!step) {
+        const step = stepData.steps.id(stepId);
+        if (!stepData) {
             return res.status(404).json({ message: 'Step nicht gefunden' });
         }
 
-        if (step) {
-
-            Object.assign(step, stepUpdates);
-            await story.save();
-            console.log("Step:", step);
-            res.status(200).json({ message: 'Step erfolgreich aktualisiert', step });
+        if (stepData) {
+            Object.assign(stepData, stepUpdates);
+            await stepData.save();
+            console.log("Step:", stepData);
+            res.status(200).json({ message: 'Step erfolgreich aktualisiert', step: stepData });
         }
     } catch (error) {
         console.error('Fehler beim Aktualisieren des Steps:', error);
@@ -80,6 +112,7 @@ router.get('/response/:intent', authMiddleware, async (req, res) => {
         const response = await Response.find({
             intent
         });
+        console.log("🚀 ~ response:", response);
         console.log("🚀 ~ intent:", intent);
         if (res.status(200)) {
 
@@ -92,5 +125,8 @@ router.get('/response/:intent', authMiddleware, async (req, res) => {
         res.status(500).json(error);
     }
 });
+
+
+
 
 export default router;
